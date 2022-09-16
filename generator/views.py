b0d38bb6221630a1b8ff8716449eadfba16b0af8
gonzalo <technozonearg@gmail.com>
2022-09-15 23:29:01 -0300

    about

--- generator/views.py
+++ generator/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
 def about(request):
-    return HttpResponse("hello")
+    return render(request, 'about.html')
